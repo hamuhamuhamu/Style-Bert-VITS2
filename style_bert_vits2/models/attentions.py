hamuhamuhamu/Style-Bert-VITS2

commit 831ac2bb8ab1e0bcd50ea71e37f502675e5d7d81

Improve: Perform in-place calculations while maintaining accuracy, improving speed and reducing memory

style_bert_vits2/models/attentions.py
@@ -115,15 +115,15 @@ class Encoder(nn.Module):
                 g = self.spk_emb_linear(g.transpose(1, 2))
                 assert g is not None
                 g = g.transpose(1, 2)
-                x = x + g
+                x.add_(g)
                 x = x * x_mask
             y = self.attn_layers[i](x, x, attn_mask, use_fp16=use_fp16)
             y = self.drop(y)
-            x = self.norm_layers_1[i](x + y)
+            x = self.norm_layers_1[i](x.add_(y))
 
             y = self.ffn_layers[i](x, x_mask)
             y = self.drop(y)
-            x = self.norm_layers_2[i](x + y)
+            x = self.norm_layers_2[i](x.add_(y))
         x = x * x_mask
         return x
 
@@ -207,15 +207,15 @@ class Decoder(nn.Module):
         for i in range(self.n_layers):
             y = self.self_attn_layers[i](x, x, self_attn_mask)
             y = self.drop(y)
-            x = self.norm_layers_0[i](x + y)
+            x = self.norm_layers_0[i](x.add_(y))
 
             y = self.encdec_attn_layers[i](x, h, encdec_attn_mask)
             y = self.drop(y)
-            x = self.norm_layers_1[i](x + y)
+            x = self.norm_layers_1[i](x.add_(y))
 
             y = self.ffn_layers[i](x, x_mask)
             y = self.drop(y)
-            x = self.norm_layers_2[i](x + y)
+            x = self.norm_layers_2[i](x.add_(y))
         x = x * x_mask
         return x
 
@@ -498,7 +498,7 @@ class FFN(nn.Module):
         if self.activation == "gelu":
             x = x * torch.sigmoid(1.702 * x)
         else:
-            x = torch.relu(x)
+            x.relu_()
         x = self.drop(x)
         x = self.conv_2(self.padding(x * x_mask))
         return x * x_mask

style_bert_vits2/models/models.py
@@ -63,11 +63,11 @@ class DurationDiscriminator(nn.Module):  # vits2
         dur = self.dur_proj(dur)
         x = torch.cat([x, dur], dim=1)
         x = self.pre_out_conv_1(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.pre_out_norm_1(x)
         x = self.drop(x)
         x = self.pre_out_conv_2(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.pre_out_norm_2(x)
         x = self.drop(x)
         x = x * x_mask
@@ -86,13 +86,13 @@ class DurationDiscriminator(nn.Module):  # vits2
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
         x = self.conv_1(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_1(x)
         x = self.drop(x)
         x = self.conv_2(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_2(x)
         x = self.drop(x)
 
@@ -310,7 +310,7 @@ class StochasticDurationPredictor(nn.Module):
         x = self.pre(x)
         if g is not None:
             g = torch.detach(g)
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
         x = self.convs(x, x_mask)
         x = self.proj(x) * x_mask
 
@@ -404,13 +404,13 @@ class DurationPredictor(nn.Module):
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
         x = self.conv_1(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_1(x)
         x = self.drop(x)
         x = self.conv_2(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_2(x)
         x = self.drop(x)
         x = self.proj(x * x_mask)
@@ -654,10 +654,10 @@ class Generator(torch.nn.Module):
     def forward(self, x: torch.Tensor, g: torch.Tensor | None = None) -> torch.Tensor:
         x = self.conv_pre(x)
         if g is not None:
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
 
         for i in range(self.num_upsamples):
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             x = self.ups[i](x)
             xs = None
             for j in range(self.num_kernels):
@@ -667,9 +667,9 @@ class Generator(torch.nn.Module):
                     xs += self.resblocks[i * self.num_kernels + j](x)
             assert xs is not None
             x = xs / self.num_kernels
-        x = F.leaky_relu(x)
+        F.leaky_relu(x, inplace=True)
         x = self.conv_post(x)
-        x = torch.tanh(x)
+        x.tanh_()
 
         return x
 
@@ -757,7 +757,7 @@ class DiscriminatorP(torch.nn.Module):
 
         for layer in self.convs:
             x = layer(x)
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             fmap.append(x)
         x = self.conv_post(x)
         fmap.append(x)
@@ -787,7 +787,7 @@ class DiscriminatorS(torch.nn.Module):
 
         for layer in self.convs:
             x = layer(x)
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             fmap.append(x)
         x = self.conv_post(x)
         fmap.append(x)
@@ -872,7 +872,7 @@ class ReferenceEncoder(nn.Module):
         for conv in self.convs:
             out = conv(out)
             # out = wn(out)
-            out = F.relu(out)  # [N, 128, Ty//2^K, n_mels//2^K]
+            F.relu(out, inplace=True)  # [N, 128, Ty//2^K, n_mels//2^K]
 
         out = out.transpose(1, 2)  # [N, Ty//2^K, 128, n_mels//2^K]
         T = out.size(1)

style_bert_vits2/models/models_jp_extra.py
@@ -69,13 +69,13 @@ class DurationDiscriminator(nn.Module):  # vits2
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
         x = self.conv_1(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_1(x)
         x = self.drop(x)
         x = self.conv_2(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_2(x)
         x = self.drop(x)
 
@@ -293,7 +293,7 @@ class StochasticDurationPredictor(nn.Module):
         x = self.pre(x)
         if g is not None:
             g = torch.detach(g)
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
         x = self.convs(x, x_mask)
         x = self.proj(x) * x_mask
 
@@ -387,13 +387,13 @@ class DurationPredictor(nn.Module):
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
         x = self.conv_1(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_1(x)
         x = self.drop(x)
         x = self.conv_2(x * x_mask)
-        x = torch.relu(x)
+        x.relu_()
         x = self.norm_2(x)
         x = self.drop(x)
         x = self.proj(x * x_mask)
@@ -659,10 +659,10 @@ class Generator(torch.nn.Module):
     def forward(self, x: torch.Tensor, g: torch.Tensor | None = None) -> torch.Tensor:
         x = self.conv_pre(x)
         if g is not None:
-            x = x + self.cond(g)
+            x.add_(self.cond(g))
 
         for i in range(self.num_upsamples):
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             x = self.ups[i](x)
             xs = None
             for j in range(self.num_kernels):
@@ -672,9 +672,9 @@ class Generator(torch.nn.Module):
                     xs += self.resblocks[i * self.num_kernels + j](x)
             assert xs is not None
             x = xs / self.num_kernels
-        x = F.leaky_relu(x)
+        F.leaky_relu(x, inplace=True)
         x = self.conv_post(x)
-        x = torch.tanh(x)
+        x.tanh_()
 
         return x
 
@@ -762,7 +762,7 @@ class DiscriminatorP(torch.nn.Module):
 
         for layer in self.convs:
             x = layer(x)
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             fmap.append(x)
         x = self.conv_post(x)
         fmap.append(x)
@@ -792,7 +792,7 @@ class DiscriminatorS(torch.nn.Module):
 
         for layer in self.convs:
             x = layer(x)
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             fmap.append(x)
         x = self.conv_post(x)
         fmap.append(x)
@@ -879,7 +879,7 @@ class WavLMDiscriminator(nn.Module):
         fmap = []
         for l in self.convs:
             x = l(x)
-            x = F.leaky_relu(x, modules.LRELU_SLOPE)
+            F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)
             fmap.append(x)
         x = self.conv_post(x)
         x = torch.flatten(x, 1, -1)
@@ -930,7 +930,7 @@ class ReferenceEncoder(nn.Module):
         for conv in self.convs:
             out = conv(out)
             # out = wn(out)
-            out = F.relu(out)  # [N, 128, Ty//2^K, n_mels//2^K]
+            F.relu(out, inplace=True)  # [N, 128, Ty//2^K, n_mels//2^K]
 
         out = out.transpose(1, 2)  # [N, Ty//2^K, 128, n_mels//2^K]
         T = out.size(1)

style_bert_vits2/models/modules.py
@@ -318,7 +318,7 @@ class ResBlock1(torch.nn.Module):
             if x_mask is not None:
                 xt = xt * x_mask
             xt = c1(xt)
-            xt = F.leaky_relu(xt, LRELU_SLOPE)
+            F.leaky_relu(xt, LRELU_SLOPE, inplace=True)
             if x_mask is not None:
                 xt = xt * x_mask
             xt = c2(xt)

commit 2378871c7ba3c941456ae5004ab254b3b2602071

Fix: Part of the model definition was moved to in-place calculations to speed up the model, which caused an error during model training
Regression introduced in commit 831ac2bb8ab1e0bcd50ea71e37f502675e5d7d81

style_bert_vits2/models/attentions.py
@@ -115,15 +115,15 @@ class Encoder(nn.Module):
                 g = self.spk_emb_linear(g.transpose(1, 2))
                 assert g is not None
                 g = g.transpose(1, 2)
-                x.add_(g)
+                x = commons.residual_add(x, g)
                 x = x * x_mask
             y = self.attn_layers[i](x, x, attn_mask, use_fp16=use_fp16)
             y = self.drop(y)
-            x = self.norm_layers_1[i](x.add_(y))
+            x = self.norm_layers_1[i](commons.residual_add(x, y))
 
             y = self.ffn_layers[i](x, x_mask)
             y = self.drop(y)
-            x = self.norm_layers_2[i](x.add_(y))
+            x = self.norm_layers_2[i](commons.residual_add(x, y))
         x = x * x_mask
         return x
 
@@ -207,15 +207,15 @@ class Decoder(nn.Module):
         for i in range(self.n_layers):
             y = self.self_attn_layers[i](x, x, self_attn_mask)
             y = self.drop(y)
-            x = self.norm_layers_0[i](x.add_(y))
+            x = self.norm_layers_0[i](commons.residual_add(x, y))
 
             y = self.encdec_attn_layers[i](x, h, encdec_attn_mask)
             y = self.drop(y)
-            x = self.norm_layers_1[i](x.add_(y))
+            x = self.norm_layers_1[i](commons.residual_add(x, y))
 
             y = self.ffn_layers[i](x, x_mask)
             y = self.drop(y)
-            x = self.norm_layers_2[i](x.add_(y))
+            x = self.norm_layers_2[i](commons.residual_add(x, y))
         x = x * x_mask
         return x
 

style_bert_vits2/models/commons.py
@@ -23,6 +23,23 @@ def init_weights(m: torch.nn.Module, mean: float = 0.0, std: float = 0.01) -> No
         m.weight.data.normal_(mean, std)  # type: ignore
 
 
+def residual_add(base: torch.Tensor, residual: torch.Tensor) -> torch.Tensor:
+    """
+    学習時は非破壊的に加算し、推論時のみ in-place 加算を行う残差加算ユーティリティ
+
+    Args:
+        base (torch.Tensor): 加算先のテンソル
+        residual (torch.Tensor): 加算するテンソル
+
+    Returns:
+        torch.Tensor: 加算後のテンソル
+    """
+
+    if torch.is_grad_enabled() and base.requires_grad:
+        return base + residual
+    return base.add_(residual)
+
+
 def get_padding(kernel_size: int, dilation: int = 1) -> int:
     """
     カーネルサイズと膨張率からパディングの大きさを計算する

style_bert_vits2/models/models.py
@@ -86,7 +86,7 @@ class DurationDiscriminator(nn.Module):  # vits2
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
         x = self.conv_1(x * x_mask)
         x.relu_()
         x = self.norm_1(x)
@@ -310,7 +310,7 @@ class StochasticDurationPredictor(nn.Module):
         x = self.pre(x)
         if g is not None:
             g = torch.detach(g)
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
         x = self.convs(x, x_mask)
         x = self.proj(x) * x_mask
 
@@ -404,7 +404,7 @@ class DurationPredictor(nn.Module):
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
         x = self.conv_1(x * x_mask)
         x.relu_()
         x = self.norm_1(x)
@@ -654,7 +654,7 @@ class Generator(torch.nn.Module):
     def forward(self, x: torch.Tensor, g: torch.Tensor | None = None) -> torch.Tensor:
         x = self.conv_pre(x)
         if g is not None:
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
 
         for i in range(self.num_upsamples):
             F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)

style_bert_vits2/models/models_jp_extra.py
@@ -69,7 +69,7 @@ class DurationDiscriminator(nn.Module):  # vits2
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
         x = self.conv_1(x * x_mask)
         x.relu_()
         x = self.norm_1(x)
@@ -293,7 +293,7 @@ class StochasticDurationPredictor(nn.Module):
         x = self.pre(x)
         if g is not None:
             g = torch.detach(g)
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
         x = self.convs(x, x_mask)
         x = self.proj(x) * x_mask
 
@@ -387,7 +387,7 @@ class DurationPredictor(nn.Module):
         x = torch.detach(x)
         if g is not None:
             g = torch.detach(g)
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
         x = self.conv_1(x * x_mask)
         x.relu_()
         x = self.norm_1(x)
@@ -659,7 +659,7 @@ class Generator(torch.nn.Module):
     def forward(self, x: torch.Tensor, g: torch.Tensor | None = None) -> torch.Tensor:
         x = self.conv_pre(x)
         if g is not None:
-            x.add_(self.cond(g))
+            x = commons.residual_add(x, self.cond(g))
 
         for i in range(self.num_upsamples):
             F.leaky_relu(x, modules.LRELU_SLOPE, inplace=True)